6097d7086e625960ccb85a980f00bd707f50c1e1
qiuaijun <wang@192.168.0.100>
2015-04-17 22:20:38 +0800

    增加定时鉴权线程

---
+++ src/weixin/InitServlet.java
@@ -0,0 +1,20 @@
+package weixin;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+public class InitServlet extends HttpServlet{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		super.init(config);
+	}
+
+}
