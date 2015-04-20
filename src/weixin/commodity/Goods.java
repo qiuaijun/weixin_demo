035d81d533ab967f2aa0c331fa1bd778471a81e0
qiuaijun <wang@192.168.0.101>
2015-04-20 21:35:51 +0800

    新增微信小店相关demo

---
+++ src/weixin/commodity/Goods.java
@@ -0,0 +1,5 @@
+package weixin.commodity;
+
+public class Goods {
+
+}
---
+++ src/weixin/commodity/GoodsService.java
@@ -0,0 +1,9 @@
+package weixin.commodity;
+
+public class GoodsService {
+	
+	public static void addGoods(Goods goods){
+		
+	}
+
+}
