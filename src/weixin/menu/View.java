e702f938d5c20883b463be6eb41a105c20f93cfe
chouaijun <chouaijun@192.168.0.104>
2015-04-21 22:55:58 +0800

    新增view类型接口的处理

---
+++ src/weixin/menu/View.java
@@ -0,0 +1,31 @@
+package weixin.menu;
+
+/**
+ * 用户点击view类型按钮后，微信客户端将会打开开发者在按钮中填写的网页URL，可与网页授权获取用户基本信息接口结合，获得用户基本信息。
+ * 
+ * @author qiuaijun
+ *
+ * @date 2015年4月21日
+ */
+public class View extends Button {
+	// 固定为view
+	private String type;
+	private String url;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+}
